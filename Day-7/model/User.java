899678503613efba3b81039442fd15c3f7dff6d3
EPradeepa <125265838+EPradeepa@users.noreply.github.com>
2024-08-05 10:13:38 +0530

    Add files via upload

---
+++ Day-7/model/User.java
@@ -0,0 +1,62 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.List;
+
+@Entity
+public class User {
+
+    @Id
+    private int id;
+
+    private String username;
+    private String password;
+
+    @OneToMany(mappedBy = "user")
+    private List<Product> products;
+
+    public User() {
+    }
+
+    public User(int id, String username, String password) { 
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    // Getters and setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+}
